bdffd64f4226712182eb62a7134c6dd09a7c2929
yago rodrigues <yagodev3@gmail.com>
2022-10-27 19:45:26 -0300

    Adição de arquivo JavaScript

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Oi, mundo :-)');
+});
+
+var port = 3001;
+
+// iniciando o processo do servidor
+app.listen(port, function() {
+  console.log(`App de Exemplo escutando na porta http://localhost:${port}/`);
+});
+//
+
+//SOMA//
+app.post('/soma', function(req, res){
+    var body = req.body;
+    var resultado = soma(body.a, body.b);
+
+    res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
+  });
+
+  function soma(a, b) {
+    return a + b;
+  }
+  
+
+  //SUBTRAÇÃO//
+  app.post('/sub', function(req,res){
+    var body = req.body;
+    var resultado = sub(body.a, body.b);
+
+    res.send(`O resultado da subtração entre ${body.a} e ${body.b} é ${resultado}`)
+  });
+
+  function sub(a, b){
+    return a - b;
+  }
+  
+
+  //MULTIPLICAÇÃO//
+
